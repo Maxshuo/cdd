ae62f227ff1d206876634a0e2428ed34727a8e58
lss <528942921@qq.com>
2019-08-21 17:07:58 +0800

    100-200

--- RNN.py
+++ RNN.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data",one_hot=True)
 
-batch_size = 100
+batch_size = 200
 n_batch = mnist.train.num_examples//batch_size
-按时交付对于撒谎dliahnsdkakaka
+
 #// weight_variable()方法用于初始化权重，以便之后调用，这里用截断正太分布进行初始化，随机加入噪声。
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
